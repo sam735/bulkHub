c9e58fad6a9d1c5eeeb93167460e2003764076a6
Sanjeev Kumar <sanjeev.kumar@corp.litmusworld.com>
2021-06-24 11:09:36 +0530

    added script

--- scripts/after_install/install_dependencies.sh
+++ scripts/after_install/install_dependencies.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 PROJECT_LOCATION=/home/ubuntu/bulkHub
-# cp /home/ubuntu/.env $PROJECT_LOCATION/.env
-# source $PROJECT_LOCATION/.env
-cd $PROJECT_LOCATION
+#cp /home/ubuntu/.env $PROJECT_LOCATION/.env
+#source $PROJECT_LOCATION/.env
+#cd $PROJECT_LOCATION
 #mkdir -p /home/ubuntu/logs
 #sudo chown --recursive ubuntu: /home/ubuntu/logs
-virtualenv -p python3 env
-source /env/bin/activate
-pip3 install -r requirements.txt
-chmod +x start.sh
-deactivate
+virtualenv -p python3 /home/ubuntu/bulkHub/env
+sudo /home/ubuntu/bulkHub/env/bin/pip3 install -r /home/ubuntu/bulkHub/requirements.txt
+chmod +x /home/ubuntu/bulkHub/start.sh
